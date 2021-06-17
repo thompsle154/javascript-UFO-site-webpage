ca8ca14039fa96b4a708aec5acde02c08d7fa435
Leanne Thompson <78496051+thompsle154@users.noreply.github.com>
2021-06-16 21:47:12 -0600

    lauched chrome

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,29 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Using the UFO dataset provided in the form of an array of JavaScript objects, append a table to the web page and add new rows of data for each UFO sighting
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+//Console.log the sightings data from data.js
+console.log(data);
+
+// Use d3 to update each cell's text with report values
+data.forEach(function(sighting) {
+    console.log(sighting);
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key, value]) {
+        console.log(key, value);
+
+        //Append a cell to the row for each value in the report object
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+//Use a date form in your HTML document and write JavaScript code
+//that will search through the date/time column to find rows that
+//match
+
+//Assign the data from data.js to a descriptive variable
